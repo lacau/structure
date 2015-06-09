4d0ca6bc245be4f5279e8843ec929a3906c45760
Lacau <llacau@gmail.com>
2015-06-09 19:27:51 -0300

    Some files indentation.

--- client/js/controllers/ModalController.js
+++ client/js/controllers/ModalController.js
@@ -1,30 +1,30 @@
-app.controller('modalCtrl',['$scope', '$modal', 'fileService', function ($scope, $modal, fileService) {
-  $scope.open = function (page) {
-    if(!page.isDirectory) {
-      fileService.openFile(page);
+app.controller('modalCtrl', function ($scope, $modal, fileService) {
+	$scope.open = function (page) {
+	if(!page.isDirectory) {
+		fileService.openFile(page);
 
-      var modalInstance = $modal.open({
-        animation: true,
-        templateUrl: 'modalContent.html',
-        controller: 'modalInstanceCtrl',
-        size: 'lg',
-        resolve: {
-          page: function () {
-            return page;
-          }
-        }
-      });
-    }
-  };
+		var modalInstance = $modal.open({
+			animation: true,
+			templateUrl: 'modalContent.html',
+			controller: 'modalInstanceCtrl',
+			size: 'lg',
+			resolve: {
+				page: function () {
+					return page;
+				}
+			}
+		});
+	}
+};
 
-  $scope.toggleAnimation = function () {
-    $scope.animationsEnabled = !$scope.animationsEnabled;
-  };
-}]);
+$scope.toggleAnimation = function () {
+	$scope.animationsEnabled = !$scope.animationsEnabled;
+	};
+});
 
 app.controller('modalInstanceCtrl', function ($scope, $modalInstance, page) {
-  $scope.page = page;
-  $scope.ok = function () {
-    $modalInstance.close();
-  };
+	$scope.page = page;
+	$scope.ok = function () {
+		$modalInstance.close();
+	};
 });
