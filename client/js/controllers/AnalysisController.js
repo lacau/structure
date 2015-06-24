7a4cb940637dc7500851455b5ab6d252d981460e
Lacau <llacau@gmail.com>
2015-06-24 00:48:40 -0300

    AnalysisController code format.

--- client/js/controllers/AnalysisController.js
+++ client/js/controllers/AnalysisController.js
@@ -1,15 +1,15 @@
 app.controller('analysisCtrl', function($scope, $resource) {
-    var _page = $resource('/api/page/' + $scope.indexMenu);
-    _page.get(function(result) {
-        $scope.page = result;
-    });
+	var _page = $resource('/api/page/' + $scope.indexMenu);
+	_page.get(function(result) {
+		$scope.page = result;
+	});
 
-    var _file = $resource('/api/file');
-    _file.query(function(result) {
-        var _analysis = $resource('/api/analysis');
-        _analysis.query(result, function(_data) {
-            $scope.analysisTable = _data[0].analysisTable;
-            $scope.basicChart = _data[0].columnChart;
-        }); 
-    });
+	var _file = $resource('/api/file');
+	_file.query(function(result) {
+		var _analysis = $resource('/api/analysis');
+		_analysis.query(result, function(_data) {
+			$scope.analysisTable = _data[0].analysisTable;
+			$scope.basicChart = _data[0].columnChart;
+		});
+	});
 });
